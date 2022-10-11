ef7b3a76e61b022c0642db21f41a1a391ab4db8e
Santiago <santi.gutierrez.restrepo@gmail.com>
2022-10-10 20:26:46 -0500

    de header a main

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import close from '../app/images/icon-hamburger.svg'
-class Test extends Component{
+class Header extends Component{
     listOptions = [
         "About", "Careers", "Events", "Products", "Support"
     ]
@@ -9,7 +9,7 @@ class Test extends Component{
         <nav className="header"><ul className="header__nav">
              {
                     this.listOptions.map((element, index) => (
-                       <div className="header__border"> <li key={index} className="header__list"> {element} </li>
+                       <div className="header__border"> <li className="header__list"> {element} </li>
                      </div> 
                     ) )
                 }
@@ -18,4 +18,4 @@ class Test extends Component{
         /></div> </nav>
       
     )}}
-    export default Test;
+    export default Header;
